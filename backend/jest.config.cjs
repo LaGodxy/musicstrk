361aa1ddac19f57e9c3082bcc7dbd7f66d62edf0
LaGodxy <algodicino@gmail.com>
2025-05-01 19:32:32 -0700

    [BUIDL Audition Onchain] BE-001: Performer Registration Endpoint

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,13 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true, tsconfig: './tsconfig.json' }],
   },
   moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/src/' }),
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
 };
